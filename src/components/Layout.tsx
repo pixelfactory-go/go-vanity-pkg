386273d65872536116cd77ef0c8ed7d73d3ae9d2
Amine Benseddik <amine@pixelfactory.io>
2024-07-03 15:41:21 +0200

    refact(html): update layout to use skeleton table

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -6,29 +6,17 @@ export const Layout = (props: { title: string; children?: any }) => {
     <head>
       <title>${props.title}</title>
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/skeleton/2.0.4/skeleton.min.css" />
+      <style>
+        .description { 
+          color: #666;
+          padding-top: 5px;
+          max-width: 400px;
+        }
+        .cell {
+          vertical-align: top;
+        }
+      </style>
     </head>
-    <style>
-      .separator {
-        margin: 0.25em 0;
-      }
-      .description { color: #666; }
-
-      /* On narrow screens, switch to inline headers. */
-      .table-header { display: none; }
-      .inline-header { font-weight: bold; }
-      @media (min-width: 550px) {
-        .table-header { display: block; }
-        .inline-header { display: none; }
-      }
-
-      @media (prefers-color-scheme: dark) {
-        body { background-color: #333; color: #ddd; }
-        a { color: #ddd; }
-        a:visited { color: #bbb; }
-          .description { color: #bbb; }
-          .separator { border-color: #666; }
-      }
-    </style>
     <body>
       <div class="container">
         ${props.children}
--- src/components/PkgList.tsx
+++ src/components/PkgList.tsx
@@ -3,30 +3,33 @@ import { Pkg } from '../config'
 
 export const PkgList = (props: { pkgs: Pkg[] }) => {
   return html`
-    <div class="row table-header">
-      <div class="five columns"><strong>Package</strong></div>
-      <div class="five columns"><strong>Source</strong></div>
-      <div class="two columns"><strong>Documentation</strong></div>
-    </div>
-    ${props.pkgs.map((pkg) => {
-      return html`<hr class="separator"/>
-        <div class="row">
-          <div class="five columns name">
-            ${pkg.modulePath}
-          </div>
-          <div class="five columns">
-            <a href="https://${pkg.repo}">${pkg.repo}</a>
-          </div>
-          <div class="two columns">
-            <a href="https://${pkg.godoc}/${pkg.modulePath}">
-            <img src="${pkg.docBadge}" alt="Go Reference" />
-            </a>
-          </div>
-          ${pkg.description ? html`<div class="row">
-            <div class="twelve columns description">
-              ${pkg.description}
-            </div>` : ''}
-        </div>`
-    })}
-  `
+  <table class="u-full-width">
+  <thead>
+    <tr>
+      <th>Packages</th>
+      <th>Sources</th>
+      <th>Docs</th>
+    </tr>
+  </thead>
+
+  <tbody>
+  ${props.pkgs.map((pkg) => {
+    return html`
+    <tr>
+      <td class="cell">
+        <b>${pkg.modulePath}</b>
+        ${pkg.description ? html`<div class="description">${pkg.description}</div>` : ''}
+      </td>
+      <td class="cell">
+        <a href="https://${pkg.repo}">${pkg.repo}</a>
+      </td>
+      <td class="cell">
+        <a href="https://${pkg.godoc}/${pkg.modulePath}">
+          <img src="${pkg.docBadge}" alt="Go Reference" />
+        </a>
+      </td>
+    </tr>`
+  })}
+  </tbody>
+  </table>`
 }
