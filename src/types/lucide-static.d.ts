f13a3fa608c56c77fcd11c4e50a4e54895e61e94
Amine Benseddik <amine@pixelfactory.io>
2026-02-05 12:14:04 +0100

    feat: switch to lucide-static

---
+++ src/types/lucide-static.d.ts
@@ -0,0 +1,8 @@
+declare module "lucide-static" {
+  export const ExternalLink: string;
+  export const FileText: string;
+  export const Github: string;
+  export const Moon: string;
+  export const Package: string;
+  export const Sun: string;
+}
